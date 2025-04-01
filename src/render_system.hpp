a516932b52460d20a45ba535fba81f73c6fbad75
Zach Chernenko <zach@chernenko.com>
2025-03-31 22:33:40 -0700

    refactor: trying to add tunnelsmall

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -123,8 +123,8 @@ class RenderSystem
 	textures_path("levels/tower.png"),
 	textures_path("levels/lab.png"),
 	textures_path("levels/under.png"),
-    textures_path("levels/tunnelsmall.png"),
 	textures_path("levels/snake.png"),
+    textures_path("levels/tunnelsmall.png"),
     // <--- Add Next level here !
     textures_path("levels/background.png")
   };
